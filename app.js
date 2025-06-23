40db857698b244db6a153df775b16efc8ead02a3
Shaina G <shainagtherapy@gmail.com>
2025-06-22 20:36:54 -0700

    objects lab exercises 1-6

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ const game = {
     { name: "pokeball", quantity: 8 },
     { name: "rare candy", quantity: 99 },
   ],
+}
   
-
-//console.dir(pokemon[99], { maxArrayLength: null })
-//console.log(game)
+console.dir(pokemon[99], { maxArrayLength: null })
+console.log(game)
 
 /*
 Exercise 3
@@ -31,8 +31,8 @@ Exercise 3
 Solve Exercise 3 here:
 */
 
-difficulty: [ "Easy" ]
-}
+game.difficulty = "Easy"
+
 console.log(game)
 
 /*
@@ -59,9 +59,9 @@ Solve Exercise 5 here:
 /*first attempt(which I learned in breakout groups isn't right:
 game.party = [pokemon[6], pokemon[38], pokemon[64], pokemon[149]] */
 
-//I chose these 3 pokemon because they are nostalgic!
+// I chose 3 nostalgic pokemon I remember from being a kid
 game.party.push(pokemon[38], pokemon[64], pokemon[88])
-console.log(game.party)
+console.log(game.party);
 
 /*
 Exercise 6
@@ -72,9 +72,212 @@ Exercise 6
 Solve Exercise 6 here:
 */
 
-game.gyms function {
-    if(difficulty < 3)
-        completed.return "true"
+game.gyms.forEach (gym =>  {
+    if (gym.difficulty < 3) {
+        gym.completed = true;
+    }
+  })
+console.log(game.gyms);
+
+// // I don't understand why any name makes the function work. 
+// // I understand the etiquette of having a plural name for an array or object so you can refer to parts singularly (ie gyms to gym). But the explanations say it can be changed to any name (next example I tried another word), so how does this work??
+
+// game.gyms.forEach (room =>  {
+//     if (room.difficulty < 3) {
+//         room.completed = true;
+//     }
+//   })
+// console.log(game.room);
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
+
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+
+
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 9 here:
+*/
+
+
+
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
+
+
+
+/*
+Exercise 11
+1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+Tips:
+For this exercise, it's okay to have a negative number of pokeballs.
+After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 11 here:
+*/
+
+
+
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 12 here:
+*/
+
+
+
+/*
+Exercise 13
+1. Create a `gymStatus` method in `game` to tally completed and incomplete gyms.
+2. How will you iterate through the `gyms` array and update the tally? Remember to log the final tally.
+
+This method should:
+  - Not accept any arguments.
+  - Initially create a constant `gymTally`, which is an object that has two 
+    properties: `completed` and `incomplete`, both of which are initially set to 0.
+  - Iterate through the objects in the `game.gyms` array and update the 
+    properties on `gymTally` as follows: 
+    - `completed` should count how many gyms in the array have a value of `true` 
+      for their `completed` property. 
+    - `incomplete` should count how many gyms in the array have a value of 
+      `false` for their `completed` property.
+  - Log the value of `gymTally`.
+  - The method should not return anything.
+
+For example, if five gym objects have a value of `true` on their `completed` property and three gym objects have a value of `false` on their `completed` property, the logged value would be: `{ completed: 5, incomplete: 3 }`.
+
+Solve Exercise 13 here:
+*/
+
+
+
+
+// extra practice:
+const coffeeOrder = {
+  drink: "cappuccino",
+  size: "12oz",
+  milk: "whole",
+  shots: 2
 }
-//game.gyms.forEach 
-console.log(game.gyms)
+
+console.log(coffeeOrder.drink)
+console.log(coffeeOrder["milk"])
+console.log(coffeeOrder["drink"])
+console.log(coffeeOrder.shots)
+
+const pet = {
+  type: "dog",
+  name: "Buddy",
+  age: 4,
+  isVaccinated: true
+}
+
+console.log(pet.name)
+console.log(pet.isVaccinated)
+
+const car = {
+  brand: "Toyota",
+  color: "blue",
+  year: 2020,
+}
+
+console.log(car["color"])
+
+const movie = {
+  title: "The Matrix",
+  rating: "R",
+}
+
+console.log(movie)
+
+movie.genre = "sci-fi"
+console.log(movie)
+
+movie.rating = "PG-13"
+console.log(movie)
+
+const person = {
+  name: "Taylor",
+  contact: {
+    email: "taylor@email.com",
+    phone: "555-1234",
+  }
+}
+console.log(person.contact.email)
+console.log(person["contact"]["email"])
+
+const dog = {
+  name: "Rex",
+  bark: function() {
+    console.log("Woof!")
+  }
+}
+dog.bark();
+
+const doggy = {
+  name: "Rex",
+  bark() {
+    console.log("Woof!")
+  }
+}
+doggy.bark()
+
+const counter = {
+  count: 5, //change this number for different answers (to get this.count += 1)
+  increment() {
+    counter.count += 1; //can use this.count since working within this same object
+  }
+}
+counter.increment();
+
+console.log(counter.count)
+
+const student = {
+  name: "Shaina",
+  sayHello () {
+    return "Hi, my name is " + student.name
+  }
+}
+console.log(student.sayHello())
